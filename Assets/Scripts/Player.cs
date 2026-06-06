e8c8e49225e6f8bf85d3dcf20a094126df60542d
PanKonKfe24 <businees.juan@gmail.com>
2026-06-06 01:46:35 -0600

    Movimiento del jugador
    Se agregó:
    - Movimiento de izquierda y derecha
    - Salto

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,103 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public PlayerInput playerInput;
+
+    [Header("Movement Variables")]
+    public float speed;
+    public float jumpForce;
+    public float jumpCutMultiplier = .5f;
+    public float normalGravity;
+    public float fallGravity;
+    public float jumpGravity;
+
+    public int facingDirection = 1;
+    public Vector2 moveInput;
+
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool isGrounded;
+
+    private void Start()
+    {
+        rb.gravityScale = normalGravity;
+    }
+
+    void Update()
+    {
+        ApplyVariableGravity();
+        CheckGrounded();
+        Flip();
+    }
+
+    void FixedUpdate()
+    {
+        float targetSpeed = moveInput.x * speed;
+        rb.linearVelocity = new Vector2(targetSpeed, rb.linearVelocityY);
+    }
+
+    void ApplyVariableGravity()
+    {
+        if(rb.linearVelocity.y < -0.1f)
+        {
+            rb.gravityScale = fallGravity;
+        }
+        else if(rb.linearVelocity.y > 0.1f)
+        {
+            rb.gravityScale = jumpGravity;
+        }
+        else
+        {
+            rb.gravityScale = normalGravity;
+        }
+    }
+
+    void CheckGrounded()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+    void Flip()
+    {
+        if(moveInput.x > 0.1f)
+        {
+            facingDirection = 1;
+        }
+        else if(moveInput.x < -0.1f)
+        {
+            facingDirection= -1;
+        }
+        transform.localScale = new Vector3(facingDirection, 1, 1);
+    }
+
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    public void OnJump (InputValue value)
+    {
+        if(value.isPressed && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+        else
+        {
+            if(rb.linearVelocity.y > 0)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * jumpCutMultiplier);
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+}
